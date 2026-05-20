0a947e68e429c00d0ef192fbc51e544107ea1baf
yoyukoka <167397586+yoyukoka@users.noreply.github.com>
2026-05-20 16:40:55 +0900

    課題2編集

--- Assets/Scripts/Meteorite.cs
+++ Assets/Scripts/Meteorite.cs
@@ -9,31 +9,28 @@ public class Meteorite : MonoBehaviour
 
     void Start()
     {
-        //課題2
-        // 初期位置を保存
-        // **ここに処理を書く**
-        
-
+        posX = this.gameObject.transform.position.x;
+        posY = this.gameObject.transform.position.y;
     }
 
     void Update()
     {
-        //課題2
-        // ゲームオーバーの時は何もしない
-        // **ここに処理を書く**
+        if (GameSystem.IsGameOver)
+        {
+            return;
+        }
 
 
         //課題2
         // 下方向に移動
         // **ここに処理を書く**
-
+        transform.position += new Vector3(0,-1,0) * speed * Time.deltaTime;
 
         //課題2
         // 隕石が消えるY座標を下回ったら非表示
         if (transform.position.y < destroyHeight)
         {
-            // **ここに処理を書く**
-
+            this.gameObject.SetActive(false);
         }
     }
 
